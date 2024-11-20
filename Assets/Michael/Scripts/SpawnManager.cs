42645a36d4b9d7ed71418343946d816b8c66ae18
Michael-elie <michael-elie@hotmail.com>
2024-11-20 17:24:49 +0100

    Update

--- Assets/Michael/Scripts/SpawnManager.cs
+++ Assets/Michael/Scripts/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject _boatPrefab;
     [SerializeField] private List<Transform> _spawnPoints;
-
+    public Camera _camera;
     private void Start() {
       InvokeRepeating(nameof(SpwanBoat),0f,5f);
     }
@@ -16,6 +16,7 @@ public class SpawnManager : MonoBehaviour
     private void SpwanBoat() {
         int num = Random.Range(0, _spawnPoints.Count);
         Instantiate(_boatPrefab, _spawnPoints[num].position, Quaternion.identity);
+      
     }
     
     
