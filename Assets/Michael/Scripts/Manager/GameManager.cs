cce10df26deb4cffa078709742f56b62fc0d07c3
sky-ark <a.renaux@nouvellesimages.xyz>
2024-12-13 08:40:14 +0100

    startin highscores with playerprefs

--- Assets/Michael/Scripts/Manager/GameManager.cs
+++ Assets/Michael/Scripts/Manager/GameManager.cs
@@ -1,20 +1,23 @@
-using System;
+using Alexandre;
+using UnityEngine;
 using DG.Tweening;
 using Michael.Scripts.Controller;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
+using TMPro;
 
 namespace Michael.Scripts.Manager
 {
     public class GameManager : MonoBehaviourSingleton<GameManager>
     {
-        
         [SerializeField] private GameObject _shopPanel;
         [SerializeField] private GameObject _gameOverPanel;
+        [SerializeField] private TextMeshProUGUI _waveSurvivedText;
+        [SerializeField] private TextMeshProUGUI _highScoresText;
         public Camera _mainCamera;
         [SerializeField] private float _shakeVibrato;
         public Canvas _canvas;
+
         private void Start()
         {
             Time.timeScale = 1;
@@ -29,40 +32,51 @@ namespace Michael.Scripts.Manager
         public void GameOver()
         {
             OpenPanel(_gameOverPanel);
+            int wavesSurvived = ScoreManager.Instance.GetWavesSurvived();
+            int[] highScores = ScoreManager.Instance.GetHighScores();
+            ScoreManager.Instance.ResetWavesSurvived(); // Réinitialiser le nombre de vagues survécues
+            ScoreManager.Instance.AddScore(wavesSurvived); // Ajouter le score actuel aux hauts scores
+            UpdateScoreDisplay(wavesSurvived, highScores);
         }
-        
+
         [ContextMenu("open shop !")]
-        public void OpenShop() {
+        public void OpenShop()
+        {
             OpenPanel(_shopPanel);
         }
- 
-        public void CloseShop() { 
+
+        public void CloseShop()
+        {
             ClosePanel(_shopPanel);
         }
 
-        public void ShakeCamera() {
-            _mainCamera.transform.DOShakePosition(0.5f,0.25f, 10);
+        public void ShakeCamera()
+        {
+            _mainCamera.transform.DOShakePosition(0.5f, 0.25f, 10);
         }
 
-        private void OpenPanel(GameObject panel) {
-            
-            panel.SetActive(true); 
+        private void OpenPanel(GameObject panel)
+        {
+            panel.SetActive(true);
             Sequence showSequence = DOTween.Sequence();
             showSequence.Append(panel.transform.DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack));
             showSequence.Join((panel.GetComponent<CanvasGroup>().DOFade(1f, 0.5f)));
             showSequence.Play();
         }
+
         private void ClosePanel(GameObject panel)
-       {
+        {
             Sequence showSequence = DOTween.Sequence();
             showSequence.Append(panel.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.OutBack));
             showSequence.Join((panel.GetComponent<CanvasGroup>().DOFade(0f, 0.5f)));
             showSequence.Play();
-            showSequence.OnComplete(()=> panel.SetActive(false)); 
+            showSequence.OnComplete(() => panel.SetActive(false));
         }
-        
-        
-        
-        
+
+        private void UpdateScoreDisplay(int wavesSurvived, int[] highScores)
+        {
+            _waveSurvivedText.text = "Waves Survived: " + wavesSurvived;
+            _highScoresText.text = "High Scores:\n1. " + highScores[0] + "\n2. " + highScores[1] + "\n3. " + highScores[2];
+        }
     }
 }
--- Assets/Michael/Scripts/Manager/WaveManager.cs
+++ Assets/Michael/Scripts/Manager/WaveManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Alexandre;
 using Michael.Scripts.Controller;
 using Michael.Scripts.Enemy;
 using TMPro;
@@ -12,21 +13,21 @@ namespace Michael.Scripts.Manager
     public class WaveManager : MonoBehaviourSingleton<WaveManager>
     {
         [Header("Enemy Progression")]
-        public int SpeedIncrement = 1; 
-        public int GoldIncrement = 25; 
+        public int SpeedIncrement = 1;
+        public int GoldIncrement = 25;
         public int HealthIncrement = 10;
-        public float BoatGoldMultiplier = 0.2f;  
-        public int StatsprogressionInterval = 3; 
+        public float BoatGoldMultiplier = 0.2f;
+        public int StatsprogressionInterval = 3;
         public int WaveprogressionInterval = 5;
-        public int _upgradeNumber = 0; 
-        
+        public int _upgradeNumber = 0;
+
         [Header("Wave Progression")]
         public List<GameObject> _spawnedBoats = new List<GameObject>();
-        public List<Transform> _spawnPoints; 
+        public List<Transform> _spawnPoints;
         public WaveData _currentWaveData;
-        [SerializeField]private int _littleBoatsIncrement  ;
-        [SerializeField]private int _balancedBoatsIncrement ;
-        [SerializeField] private int _bigBoatsIncrement = 0 ;
+        [SerializeField] private int _littleBoatsIncrement;
+        [SerializeField] private int _balancedBoatsIncrement;
+        [SerializeField] private int _bigBoatsIncrement = 0;
         [SerializeField] private List<BoatType> _boat = new List<BoatType>();
         [SerializeField] private List<GameObject> _boatsToSpawn = new List<GameObject>();
         [SerializeField] private float _spawnInterval = 5f;
@@ -36,32 +37,32 @@ namespace Michael.Scripts.Manager
         private int _boatsWithGoldGenerated = 0; // Nombre de bateaux riches générés
         private int _spawnIndex;
         private Transform _lastSpawnPoint;
-        private int _littleBoatsBonus = 0 ;
-        private int _balancedBoatsBonus = 0 ;
-        private int _bigBoatsBonus = 0 ;
+        private int _littleBoatsBonus = 0;
+        private int _balancedBoatsBonus = 0;
+        private int _bigBoatsBonus = 0;
         private int _currentWave = 0;
-        
-        
-        private void Start() {
+
+        private void Start()
+        {
             _currentWaveData = _waveData[_currentWave];
             StartWave();
         }
-        
-        
+
         private void FixedUpdate()
         {
-            if (_spawnTimer <= 0) {
-                if (_boatsToSpawn.Count > 0) {
-                    
+            if (_spawnTimer <= 0)
+            {
+                if (_boatsToSpawn.Count > 0)
+                {
                     _spawnIndex = Random.Range(0, _spawnPoints.Count);
-                   if(_lastSpawnPoint == _spawnPoints[_spawnIndex]) {
-                        
+                    if (_lastSpawnPoint == _spawnPoints[_spawnIndex])
+                    {
                         _spawnIndex = Random.Range(0, _spawnPoints.Count);
                     }
                     _lastSpawnPoint = _spawnPoints[_spawnIndex];
-                    GameObject boat = Instantiate(_boatsToSpawn[0],_lastSpawnPoint.position, Quaternion.identity);
+                    GameObject boat = Instantiate(_boatsToSpawn[0], _lastSpawnPoint.position, Quaternion.identity);
                     BoatEnemy boatEnemy = boat.GetComponent<Enemy.BoatEnemy>();
-                    
+
                     boatEnemy.InitializeBoatStats();
                     float currentGoldBoatRatio = (float)_boatsWithGoldGenerated / _boatsToSpawn.Count;
                     if (currentGoldBoatRatio < _currentWaveData.BoatWithGoldPourcent)
@@ -71,90 +72,81 @@ namespace Michael.Scripts.Manager
                         boat.GetComponent<Enemy.BoatEnemy>().SetGoldOnBoat(goldOnBoat);
                         _boatsWithGoldGenerated++;
                     }
-                    
-                    
+
                     _boatsToSpawn.RemoveAt(0);
                     _spawnedBoats.Add(boat);
-                   _spawnTimer = _spawnInterval;
+                    _spawnTimer = _spawnInterval;
 
-                    if (_spawnIndex + 1 <= _spawnPoints.Count-1) {
+                    if (_spawnIndex + 1 <= _spawnPoints.Count - 1)
+                    {
                         _spawnIndex++;
                     }
-                    else {
-                        _spawnIndex = 0; 
+                    else
+                    {
+                        _spawnIndex = 0;
                     }
                 }
             }
-            else {
+            else
+            {
                 _spawnTimer -= Time.fixedDeltaTime;
             }
 
-            if ( _spawnedBoats.Count <= 0 && _boatsToSpawn.Count <= 0) { 
-                
-               //Invoke(nameof(EndWave),1f);
-               EndWave();
+            if (_spawnedBoats.Count <= 0 && _boatsToSpawn.Count <= 0)
+            {
+                EndWave();
             }
         }
-        
+
         [ContextMenu("StartWave !")]
-        public void StartWave() {
-            
-            
-            if (_currentWave > (_waveData.Count-1) * WaveprogressionInterval) {
-                
+        public void StartWave()
+        {
+            if (_currentWave > (_waveData.Count - 1) * WaveprogressionInterval)
+            {
                 _littleBoatsBonus += _littleBoatsIncrement;
                 _balancedBoatsBonus += _balancedBoatsIncrement;
                 _bigBoatsBonus += _bigBoatsIncrement;
             }
             _spawnTimer = 0;
-           
-            // ui alerte 
+
+            // ui alerte
             _currentWave++;
             _waveText.text = "Vague : " + _currentWave;
-            
-             if (_currentWave % WaveprogressionInterval == 0 ) {
-                
+
+            if (_currentWave % WaveprogressionInterval == 0)
+            {
                 _currentWaveData = _waveData[_currentWave / WaveprogressionInterval];
-                // nouvelle vague 
-             }
-           /*  if (_currentWave % StatsprogressionInterval == 0)
-             {
-                 _upgradeNumber++;
-                 Debug.Log("stats wave !");
-             }*/
-           
+                // nouvelle vague
+            }
+
             if (_currentWave > 1)
             {
                 _upgradeNumber++;
-            } 
+            }
             GenerateEnemy();
-            
         }
 
-        private void GenerateEnemy() {
-
-                for (int i = 0; i < _currentWaveData.LittleBoatCount + _littleBoatsBonus; i++)
-                {
-                    _boatsToSpawn.Add(_boat[0].BoatPrefab);
-                }
-                for (int i = 0; i < _currentWaveData.BalancedBoatCount + _balancedBoatsBonus; i++)
-                {
-                    _boatsToSpawn.Add(_boat[1].BoatPrefab);
-                }
-                for (int i = 0; i < _currentWaveData.BigBoatCount + _bigBoatsBonus; i++)
-                {
-                    _boatsToSpawn.Add(_boat[2].BoatPrefab);
-                }
-
+        private void GenerateEnemy()
+        {
+            for (int i = 0; i < _currentWaveData.LittleBoatCount + _littleBoatsBonus; i++)
+            {
+                _boatsToSpawn.Add(_boat[0].BoatPrefab);
+            }
+            for (int i = 0; i < _currentWaveData.BalancedBoatCount + _balancedBoatsBonus; i++)
+            {
+                _boatsToSpawn.Add(_boat[1].BoatPrefab);
+            }
+            for (int i = 0; i < _currentWaveData.BigBoatCount + _bigBoatsBonus; i++)
+            {
+                _boatsToSpawn.Add(_boat[2].BoatPrefab);
+            }
         }
-        private void EndWave() {
-            
+
+        private void EndWave()
+        {
             _boatsWithGoldGenerated = 0;
             GameManager.Instance.OpenShop();
+            ScoreManager.Instance.IncrementWavesSurvived(); // Informer le ScoreManager de la vague survécue
         }
-        
-       
-        
-       
     }
 }
